cff1399ab7b514be722b2817434ae253b665ea0d
Patho <andrewlykk@gmail.com>
2022-03-21 21:56:01 -0400

    lb07

--- lb07/clone.bat
+++ lb07/clone.bat
@@ -1,1 +1,1 @@
-git clone https://github.com/Path0/Vocareum && cd ./Vocareum && cd ./lb07 && cp hw04.c ./../../lb07.c && cd ~ && rm -r -f ./Vocareum
+git clone https://github.com/Path0/Vocareum && cd ./Vocareum && cd ./lb07 && cp lb07.c ./../../lb07.c && cd ~ && rm -r -f ./Vocareum
--- lb07/lb07.c
+++ lb07/lb07.c
@@ -4,7 +4,7 @@
 int cardValue(int, int*);
 void printCardName(int);
 void printCardSuit(int);
-void calculateTotal(int*, int, int*);
+void calculateTotal(int*, int, int*, int*, int*);
 void printNominals(int);
 int main()
 {
@@ -12,43 +12,57 @@ int main()
 
   int seed;
   int num11Aces;
-  int num1Aces;
-
+  int hold;
+  int win;
+  int card;
+  int total;
+  int cardNumber;
+  int currentCardValue;
 
   printf("Enter the seed value -> ");
   scanf("%d", &seed);  
 
   srand(seed);
 
-  int card;
-  int total;
-  int cardNumber;
-  int currentCardValue;
-
   total = 0;
   cardNumber = 1;
+  hold = 0;
+  win = 0;
+
   printf("\n");
-  while(total < 17)
+  while((total < 17) && (hold == 0))
   {
     card = rand() % (52);
-    
+    currentCardValue = cardValue(card, &num11Aces);
+    calculateTotal(&total, currentCardValue, &num11Aces, &hold, &win);
+
+    if(hold) // if the total is too high don't print anything
+    {
+      continue;
+    }
+
     printf("The ");
     printNominals(cardNumber);
     printf(" card is #: %d (", (card + 1));
     printCardName(card);
     printf(" of ");
     printCardSuit(card);
-
-    currentCardValue = cardValue(card, &num11Aces);
-    calculateTotal(&total, currentCardValue, &num11Aces);
-    
     printf(") Current score: %d\n", total);
 
     cardNumber++;
   }
 
+  if(win)
+  {
+    printf("\nDealer wins!");
+  }
+  else
+  {
+    printf("\nDealer holds.");
+  }
 
 
+  return 0;
 }
 
 int cardValue(int card, int *num11Aces)
@@ -124,17 +138,28 @@ void printCardSuit(int card)
   }
 }
 
-void calculateTotal(int *total, int cardValue, int *num11Aces)
+void calculateTotal(int *total, int cardValue, int *num11Aces, int *hold, int *win)
 {
   if((*total + cardValue > 21) && (*num11Aces != 0)) // if the value would be over 21 and there is an ace
   {
     *total += cardValue;
     *total -= 10; // turns the 11 ace into a 1 ace
-    *num11Aces--; // the number of usable aces goes down 1
+    *num11Aces -= 1; // the number of usable aces goes down 1
+    *hold = 1;
   }
   else
   {
     *total += cardValue;
+  }
+
+  if(*total >= 21)
+  {
+    *hold = 1;
+  }
+  
+  if(*total == 21)
+  {
+    *win = 1;
   }
 }
 
