7d7082b20613a013a69c97cccf1ef1d3f2725f21
Patho <andrewlykk@gmail.com>
2022-03-22 23:39:51 -0400

    1

--- hw05/hw05.c
+++ hw05/hw05.c
@@ -14,6 +14,16 @@ int main()
 
   inputs(&odds, &startingNumber, &endingNumber);
 
+  if(odds)
+  {
+    printf("\nOdd Boiler numbers: ");
+  }
+  else
+  {
+    
+    printf("\nEven Boiler numbers: ");
+  }
+
   for(x = startingNumber; x <= endingNumber; x++)
   {
     currentBoilerNumber = boilerNumber(x);
@@ -42,7 +52,7 @@ int boilerNumber(int n)
 {
   int number;
 
-  number = (pow(n, 2) + n) / 2;
+  number = (pow(n, 2) + n) / 2; // this is equivalent to summing all integers from 1 to n. 
 
   return number;
 }
@@ -51,12 +61,37 @@ void inputs(int *odds, int *startingNumber, int *endingNumber)
 {
   printf("Enter 0 for even or 1 for odd values -> ");
   scanf("%d", odds);
+  
+  while((*odds != 0) && (*odds != 1))
+  {
+    printf("\nError! Select a valid option!!\n\n");
+
+    printf("Enter 0 for even or 1 for odd values -> ");
+    scanf("%d", odds);
+  }
 
   printf("Enter starting value -> ");
   scanf("%d", startingNumber);
 
+  while(*startingNumber < 1)
+  {
+    printf("\nError! Starting value must be positive!\n\n");
+
+    printf("Enter starting value -> ");
+    scanf("%d", startingNumber);
+  }
+
   printf("Enter ending value -> ");
   scanf("%d", endingNumber);
 
-  printf("\nEven Boiler numbers: ");
+  while(*endingNumber <= *startingNumber)
+  {
+    printf("Error! Ending value must be > %d", *startingNumber);
+
+    printf("Enter ending value -> ");
+    scanf("%d", endingNumber);
+
+  }
+
+  
 }
