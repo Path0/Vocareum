663e38745ff2a015961fb91de808d0452c0e7898
Patho <andrewlykk@gmail.com>
2022-03-23 13:47:16 -0400

    1

--- lb08/lb08.c
+++ lb08/lb08.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
 void inputs(int*);
+int changes(long);
 
 int main()
 {
   int sequence;
 
   inputs(&sequence);
+
+  changes(sequence);
 }
 
 void inputs(int *sequence)
@@ -14,4 +18,26 @@ void inputs(int *sequence)
   printf("Enter the initial configuration -> ");
   scanf("%ld", sequence);
   printf("\n");
+}
+
+int changes(long sequence)
+{
+  int numChanges;
+  int length;
+  int x;
+  int previousNumber;
+  int currentNumber;
+
+  length = log10(sequence) + 1;
+
+  currentNumber = sequence % 10;
+  previousNumber = !currentNumber;
+
+  for(x = 2; x <= length + 1; x++)
+  {
+    currentNumber = sequence % (x * 10);
+    
+  }
+  
+  return numChanges;
 }
