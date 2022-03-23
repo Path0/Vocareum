f3eb4b2e73c292de914367a8bfa3cd0355481009
Patho <andrewlykk@gmail.com>
2022-03-23 13:39:02 -0400

    1

--- lb08/lb08.c
+++ lb08/lb08.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+void inputs(int*);
+
 int main()
 {
-  
+  int sequence;
+
+  inputs(&sequence);
+}
+
+void inputs(int *sequence)
+{
+  printf("Enter the initial configuration -> ");
+  scanf("%ld", sequence);
+  printf("\n");
 }
