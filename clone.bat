8bf8dd6e0593faae42d02462ce4675b0cd716aad
Patho <andrewlykk@gmail.com>
2022-03-30 13:42:50 -0400

    1

---
+++ clone.bat
@@ -0,0 +1,7 @@
+git clone https://github.com/Path0/Vocareum && cd ./Vocareum && cd ./lb09 && cp -f lb09.c ./../../lb09.c && cd ~ && rm -r -f ./Vocareum
+
+git clone https://github.com/Path0/Vocareum && cd ./Vocareum && cd ./lb09 && cp -f lb09.c ./../../lb09.c && cd ~ && rm -r -f ./Vocareum && gcc lb09.c && ./a.out
+
+
+echo 'git clone https://github.com/Path0/Vocareum && cd ./Vocareum && cd ./lb09 && cp -f lb09.c ./../../lb09.c && cd ~ && rm -r -f ./Vocareum' >> clone && chmod +x clone && ./clone 
+
