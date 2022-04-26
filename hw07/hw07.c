f9207523a3d74179e777166bd44dab5478f1268f
Patho <andrewlykk@gmail.com>
2022-04-25 21:19:24 -0400

    1

--- hw07/hw07.c
+++ hw07/hw07.c
@@ -14,6 +14,10 @@ void fillArray(int*, int);
 void sortArray(int*);
 void largestValues(int*);
 void smallestValues(int*);
+int findMax(int, int*);
+int ctZero(int);
+int appendMax(int, int);
+int removeMax(int, int, int);
 
 int main()
 {
@@ -26,6 +30,7 @@ int main()
   sortArray(data);
   largestValues(data);
   smallestValues(data);
+
 
   return 0;
 }
@@ -65,7 +70,7 @@ int getRange()
     
     if(range <= 0)
     {
-      printf("\nError! Power of ten must be positive!\n");
+      printf("\nError! Power of ten must be positive!!\n");
     }
     
   } while((range > 8) || (range <= 0));
@@ -75,36 +80,110 @@ int getRange()
 
 int sortInt(int number)
 {
-  // sorts an integer by digit largest to smallest. 
-  int size;
-  int index;
-  int *digits;
-  int sortedNumber;
+  int numberCopy;
+  int sorted;
+  int max;
+  int maxLoc;
+  int zeroCount;
+  
+  max = 0;
+  sorted = 0;
+  numberCopy = number;
+  zeroCount = ctZero(numberCopy);
 
-  size = (int) floor(log10(number)) + 1; // length of the number
-  digits = (int*) malloc(sizeof(int) * size); // makes an array of integers the size of the number
-  sortedNumber = 0;
-
-  for(index = 0; index < size; index++)
+  while(numberCopy > 0)
   {
-    // make an array of dynamic size and put all the digits into that and bubble sort
-    // this way no repeat values are lost and bubble sort is easy
-    setValue(digits, index, number % 10);
-    number /= 10;
+    max = findMax(numberCopy, &maxLoc);
+    sorted = appendMax(sorted, max);
+    numberCopy = removeMax(numberCopy, max, maxLoc);
   }
 
-  bubbleSort(digits, size);
+  sorted = sorted * (int)pow(10, zeroCount);
 
-  // sorted number is just the digit times 10^index value added together
-  for(index = size - 1; index >= 0; index--)
-  {
-    sortedNumber += getValue(digits, index) * pow(10, index);
-  }
-
-  return sortedNumber;
+  return sorted;
 }
 
-void bubbleSort(int *array, int arraySize)
+
+int ctZero(int number)
+{
+  int ct = 0;
+
+  while(number > 0)
+  {
+    if(number % 10 == 0)
+    {
+      ct++;
+    }
+    number = number / 10;
+  }
+  return ct;
+}
+
+int findMax(int number, int *maxLocation)
+{
+  int max;
+  int location;
+
+  max = 0;
+  location = 0;
+
+  while(number > 0)
+  {
+    if(number % 10 > max)
+    {
+      max = number % 10;
+      *maxLocation = location;
+    }
+
+    number = number / 10;
+    location++;
+  }
+
+  return max;
+}
+
+int appendMax(int sorted, int max)
+{
+  return(sorted * 10 + max);
+}
+
+int removeMax(int n, int max, int maxLoc)
+{
+  return(n - max * (int)pow(10, maxLoc));
+}
+
+
+// int sortInt(int number, int size)
+// {
+//   // sorts an integer by digit largest to smallest. 
+//   int index;
+//   int digits[size];
+//   int sortedNumber;
+
+//   //size = (int) floor(log10(number)) + 1; // length of the number
+//   //digits = (int*) malloc(sizeof(int) * size); // makes an array of integers the size of the number
+//   sortedNumber = 0;
+
+//   for(index = 0; index < size; index++)
+//   {
+//     // make an array of dynamic size and put all the digits into that and bubble sort
+//     // this way no repeat values are lost and bubble sort is easy
+//     digits[index] = number % 10;
+//     number /= 10;
+//   }
+
+//   bubbleSort(digits, size);
+
+//   // sorted number is just the digit times 10^index value added together
+//   for(index = size - 1; index >= 0; index--)
+//   {
+//     sortedNumber += digits[index] * pow(10, index);
+//   }
+
+//   return sortedNumber;
+// }
+
+void bubbleSort(int array[], int arraySize)
 {
   int numPasses; // lcv that controls number of passes
   int lcv; // loop control variable for sorting
@@ -114,34 +193,36 @@ void bubbleSort(int *array, int arraySize)
   {
     for(lcv = 0; lcv < arraySize - numPasses; lcv++)
     {
-      if(getValue(array, lcv) > getValue(array, lcv + 1))
+      if(array[lcv] > array[lcv + 1])
       {
-        temp = getValue(array, lcv);
-        setValue(array, lcv, getValue(array, lcv + 1));
-        setValue(array, lcv + 1, temp);
+        temp = array[lcv];
+        array[lcv] = array[lcv + 1];
+        array[lcv + 1] = temp;
       } // end of if
     } // end of comparison loop
   } // end of passes loop
 
 }
 
-void fillArray(int* array, int range)
+void fillArray(int array[], int range)
 {
   int index;
   for(index = 0; index < ARRAY_SIZE; index++)
   {
-    setValue(array, index, rand() % range);
+    array[index] = rand() % range;
   }
 }
 
-void sortArray(int* array)
+void sortArray(int array[])
 {
   int index;
+  int size;
   int sortedValue;
   for(index = 0; index < ARRAY_SIZE; index++)
   {
-    sortedValue = sortInt(getValue(array, index));
-    setValue(array, index, sortedValue);
+    size = (int) floor(log10(array[index])) + 1;
+    sortedValue = sortInt(array[index]);
+    array[index] = sortedValue;
   }
 
   bubbleSort(array, ARRAY_SIZE);
